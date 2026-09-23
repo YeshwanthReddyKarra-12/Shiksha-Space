7aae618544ab1ed134ebd3a268553d608fce0296
Yeshwanth Reddy Karra <yeshwanthreddykarra@gmail.com>
2026-09-23 10:06:16 +0530

    qwerty

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,3 +1,10 @@
 const express = require("express")
-const {login, register}=request("..\\controllers\authController")
+const { login, register } = require("./authController")
 
+const authRoute = express.Router()
+
+authRoute.post("/login", login)
+
+authRoute.post("/register", register)
+
+module.exports = authRoute
